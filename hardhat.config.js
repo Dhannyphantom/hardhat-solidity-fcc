7cc97a9550d9777fe742237d4b1fd991cfb00a92
Dhannyphantom <youngskillzzz@gmail.com>
2022-11-05 16:06:37 +0100

    Deployed and verified contract to goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
     solidity: "0.8.17",
     defaultNetwork: "hardhat",
     networks: {
-        rinkeby: {
-            url: process.env.GOERLI_RPC_URL,
-            accounts: [process.env.PRIVATE_KEY],
-            chainId: 4,
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
         },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -9,6 +9,27 @@ async function main() {
     await simpleStorage.deployed()
     console.log("Contract Deployed!")
     console.log(`Contract Address: ${simpleStorage.address}`)
+
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        try {
+            await simpleStorage.deployTransaction.wait(6)
+            await verify(simpleStorage.address, [])
+        } catch (e) {
+            if (e.message.toLowerCase().includes("already verified")) {
+                console.log("Contract Already Verified")
+            } else {
+                console.error(e)
+            }
+        }
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    await run("verify:verify", {
+        address: contractAddress,
+        contractArguments: args,
+    })
 }
 
 main().catch((error) => {
