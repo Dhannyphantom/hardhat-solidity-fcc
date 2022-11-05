7192bdb4ddcfde827b5faaa870f250585375c814
Dhannyphantom <youngskillzzz@gmail.com>
2022-11-05 15:40:37 +0100

    Deployed SimpleStorage contract to Goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+    solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: process.env.GOERLI_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: 4,
+        },
+    },
+}
