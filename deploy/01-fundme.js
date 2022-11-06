bc870d55c44f2fceb0f075e34994b9fbb026210d
Dhannyphantom <youngskillzzz@gmail.com>
2022-11-06 18:49:33 +0100

    Successfully deployed FundMe contract along with a MockV3Aggregator contract price feed address

--- deploy/01-fundme.js
+++ deploy/01-fundme.js
@@ -1,10 +1,26 @@
+const { network } = require("hardhat")
+const { networkConfig, DEV_CHAINS } = require("../helper-hardhat-config")
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
 
-    const fundMe = await deploy("FundMe", {
+    // DYNAMICALLY GET THE PRICE_FEED_ADDRESS BASED ON THE CHAIN ID
+    let ethUsdPriceFeedAddress
+
+    if (DEV_CHAINS.includes(network.name)) {
+        // development chain, so grab address from deployed MockV3Aggregator
+        const MockV3Aggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = MockV3Aggregator.address
+    } else {
+        // testnets or mainnets
+        ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeedAddress
+    }
+
+    await deploy("FundMe", {
         from: deployer,
-        args: [],
+        args: [ethUsdPriceFeedAddress],
         log: true,
     })
 
