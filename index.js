81f7e3233846b6b652cd28951448ec8d604a9668
Dhannyphantom <youngskillzzz@gmail.com>
2022-11-14 16:55:47 +0100

    Withdraw function added and concludes this lesson

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import { abi, contractAddress } from './constants.js'
 import { ethers } from './ethers-5.1.esm.min.js'
 
-const connectBtn = document.querySelector('#connectBtn')
-const fundBtn = document.querySelector('#fundBtn')
+const connectBtn = document.getElementById('connectBtn')
+const fundBtn = document.getElementById('fundBtn')
 const getBalanceBtn = document.getElementById('balanceBtn')
-const amountInput = document.querySelector('#amount')
+const amountInput = document.getElementById('amount')
+const withdrawBtn = document.getElementById('withdraw')
 connectBtn.onclick = connect
 fundBtn.onclick = fund
 getBalanceBtn.onclick = getBalance
+withdrawBtn.onclick = withdraw
 
 async function connect() {
     if (typeof window.ethereum !== 'undefined') {
@@ -45,7 +47,18 @@ async function getBalance() {
     console.log(ethers.utils.formatEther(balance))
 }
 
+async function withdraw() {
+    try {
+        console.log('Withdrawing ETH...')
+        const withdrawTx = await contract.withdraw()
+        await listenForTransactionMine(withdrawTx, provider)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 function listenForTransactionMine(transactionRespone, provider) {
+    console.log(`Mining ${transactionRespone.hash}`)
     return new Promise((resolve, reject) => {
         provider.once(transactionRespone.hash, (transactionReciept) => {
             console.log(
