9b9e3c18380c87cf90febd188bcecdb7a423c937
Dhannyphantom <youngskillzzz@gmail.com>
2022-11-14 15:57:25 +0100

    Added a fund function and listener for block miners

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+import { abi, contractAddress } from './constants.js'
 import { ethers } from './ethers-5.1.esm.min.js'
 
 const connectBtn = document.querySelector('#connectBtn')
 const fundBtn = document.querySelector('#fundBtn')
 connectBtn.onclick = connect
 fundBtn.onclick = fund
-
-console.log(ethers)
 
 async function connect() {
     if (typeof window.ethereum !== 'undefined') {
@@ -17,10 +16,31 @@ async function connect() {
     }
 }
 
-async function fund(ethAmount) {
+async function fund() {
+    const ethAmount = '10'
     if (typeof window.ethereum !== 'undefined') {
-        console.log(ethAmount)
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+
+        console.log(`Funding contract with ${ethAmount}ETH`)
+        const fundTx = await contract.fund({
+            value: ethers.utils.parseEther(ethAmount),
+        })
+        await listenForTransactionMine(fundTx, provider)
+        console.log('Funded!')
     } else {
         alert('Please install metamask')
     }
+}
+
+function listenForTransactionMine(transactionRespone, provider) {
+    return new Promise((resolve, reject) => {
+        provider.once(transactionRespone.hash, (transactionReciept) => {
+            console.log(
+                `Completed with ${transactionReciept.confirmations} confirmations`
+            )
+            resolve()
+        })
+    })
 }
